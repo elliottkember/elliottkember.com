28d06a73ac1f46aae0e79bb48898e5a4f1a4aefc
Elliott Kember <elliott.kember@gmail.com>
2017-01-25 22:36:15 +0000

    More homepage

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -7,41 +7,44 @@ export default class Homepage extends Component {
   render() {
     return (
       <div>
-        <div className={classnames(s.section)}>
-          <p>hello <br />
-            I am Elliott
-          </p>
-        </div>
+        <div className={s.border} />
+        <div className={s.sections}>
+          <div className={classnames(s.section)}>
+            <p>hello <br />
+              I am Elliott
+            </p>
+          </div>
 
-        <div className={classnames(s.section)}>
-          <p>I make things</p>
-        </div>
+          <div className={classnames(s.section)}>
+            <p>I make things</p>
+          </div>
 
-        <div className={classnames(s.section)}>
-          <p>like software</p>
-        </div>
+          <div className={classnames(s.section)}>
+            <p>like software</p>
+          </div>
 
-        <div className={classnames(s.section)}>
-          <p>and art</p>
-        </div>
+          <div className={classnames(s.section)}>
+            <p>and art</p>
+          </div>
 
-        <div className={classnames(s.section)}>
-          <p>and jokes</p>
-        </div>
+          <div className={classnames(s.section)}>
+            <p>and jokes</p>
+          </div>
 
-        <div className={classnames(s.section)}>
-          <p>I work at ueno.</p>
-        </div>
+          <div className={classnames(s.section)}>
+            <p>I work at ueno.</p>
+          </div>
 
-        <div className={classnames(s.section)}>
-          <p>(I also have worked at other places)</p>
-        </div>
+          <div className={classnames(s.section)}>
+            <p>(I also have worked at other places)</p>
+          </div>
 
-        <div className={classnames(s.section)}>
-          <p>
-            Talk to me<br />
-            elliott.kember@gmail.com
-          </p>
+          <div className={classnames(s.section)}>
+            <p>
+              Talk to me<br />
+              elliott.kember@gmail.com
+            </p>
+          </div>
         </div>
       </div>
     );
